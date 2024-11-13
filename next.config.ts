a52d1cf0e64e73bdae44d58e78945600109637ac
jung602 <kuhaku05@naver.comm>
2024-11-14 00:41:43 +0900

    1114

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  webpack: (config) => {
+    // GLB/GLTF 파일 처리를 위한 규칙 추가
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/chunks/[path][name].[hash][ext]'
+      }
+    });
+
+    // draco 압축을 위한 worker 설정 (선택사항)
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'three-stdlib': require.resolve('three-stdlib'),
+    };
+
+    return config;
+  },
+  // Three.js 관련 패키지들을 위한 transpile 설정
+  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
 };
 
 export default nextConfig;
--- pages/models/altblock.tsx
+++ pages/models/altblock.tsx
@@ -3,6 +3,8 @@ import * as THREE from 'three'
 import React from 'react'
 import { useGLTF } from '@react-three/drei'
 import { GLTF } from 'three-stdlib'
+import { GroupProps } from '@react-three/fiber'
+import dynamic from 'next/dynamic'
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -13,7 +15,9 @@ type GLTFResult = GLTF & {
   }
 }
 
-export function Altblock(props: JSX.IntrinsicElements['group']) {
+// 동적으로 로드될 컴포넌트 생성
+const AltblockComponent = (props: GroupProps) => {
+  const { useGLTF } = require('@react-three/drei')
   const { nodes, materials } = useGLTF('./gltf/altblock.glb') as GLTFResult
   
   return (
@@ -27,4 +31,19 @@ export function Altblock(props: JSX.IntrinsicElements['group']) {
   )
 }
 
-useGLTF.preload('./gltf/altblock.glb')
+// SSR을 비활성화하고 동적 임포트 설정
+const Altblock = dynamic(
+  () => Promise.resolve(AltblockComponent),
+  {
+    ssr: false,
+  }
+)
+
+// 컴포넌트 내보내기
+export { Altblock }
+
+// 모델 프리로드 (브라우저 환경에서만 실행)
+if (typeof window !== 'undefined') {
+  const { useGLTF } = require('@react-three/drei')
+  useGLTF.preload('./gltf/altblock.glb')
+}
